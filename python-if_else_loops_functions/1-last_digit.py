9a90f03eff9bf8c7a44458fb2992541a1e716f8b
AudreyLican <audrey.lican@gmail.com>
2024-01-25 23:29:27 +0100

    added nex cond. on greater than 6 and move cond. 0 to end of code

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,9 @@ if number > 0:
 else:
     last_digit = ((number * -1) % 10) * -1
 
-if last_digit == 0:
-    print("Last digit of {} is {} and is 0\n".format(number, last_digit))
-elif last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5\n".format(number, last_digit))
-else:
-    print("Last digit of {} is {} and is less than 6 and not 0\n".format(number, last_digit))
+if last_digit > 5:
+    print("Last digit of {0} is {1} and is greater than 5\n".format(number, last_digit))
+elif last_digit < 6 and last_digit != 0:
+    print("Last digit of {0} is {1} and is less than 6 and not 0\n".format(number, last_digit))
+elif last_digit == 0:
+    print("Last digit of {0} is {1} and is 0\n".format(number, last_digit))

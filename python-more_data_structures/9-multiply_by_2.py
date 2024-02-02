68ed952f79bf4f10bec2d40dc9b387e590f6d698
AudreyLican <audrey.lican@gmail.com>
2024-02-02 23:09:22 +0100

    deleted comments for empty dic and change name variable new_dic

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    #create empty dictionnary to store multiplied values
-    new_dic = {}
+    new_dictionary = {}
     #iterate over key-value in a_dictionnary
     for key, value in a_dictionary.items():
-        new_dic[key] = value * 2
-    return new_dic
+        new_dictionary[key] = value * 2
+    return new_dictionary

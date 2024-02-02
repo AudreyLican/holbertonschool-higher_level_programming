af9bbf861ef4f2e2b023ed4e00e97f30d289788a
AudreyLican <audrey.lican@gmail.com>
2024-02-02 23:13:52 +0100

    changed comment line of place

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    # using & operator, to find intersection btw the two set(common elements)
     return set_1 & set_2
-#using & to find intersection btw the two set(common elements)

2b1814288abc225c0ecd4d4503cd6d28df27eb8a
AudreyLican <audrey.lican@gmail.com>
2024-02-02 14:50:30 +0100

    change nam var. max_value for max & i for nb

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == "":
+    if not my_list:
         return None
 
-    max_value = my_list[0]
-    for i in my_list:
-        if max_value < i:
-            max_value = i
-    return max_value
+    max = my_list[0]
+    for nb in my_list:
+        if max < nb:
+            max = nb
+    return max

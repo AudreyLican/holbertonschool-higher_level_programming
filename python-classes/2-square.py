98e99a1bef62c56baa4bddca4f489d9154b673de
AudreyLican <audrey.lican@gmail.com>
2024-02-06 14:59:38 +0100

    create ex 2 square

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a square class"""
+class Square:
+    
+    """Define square attribute"""
+    def __init__(self, size=0):
+        
+        """
+        Initializing the square.
+        Arg:
+            size (int): the size of the square
+            default to 0.
+        """
+        if not isinstance(size, int):
+            raise Exception("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

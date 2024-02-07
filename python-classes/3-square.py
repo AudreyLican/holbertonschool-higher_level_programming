7a422bba33c17652578ef67c4e4477d62c58a36e
AudreyLican <audrey.lican@gmail.com>
2024-02-07 20:27:35 +0100

    docstrings fixed

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 """Define a square class"""
+
+
 class Square:
-    
     """Define square attribute"""
+
     def __init__(self, size=0):
-        
         """
         Initializing the square.
         Arg:
             size (int): the size of the square
             default to 0.
+
+        Raises:
+            TypeError: if size is not a integer
+            ValueError: if size is less than 0
         """
+
         if not isinstance(size, int):
-            raise Exception("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     """Public instance method"""
     def area(self):
-        """Returns:
-            The current square size"""
+        """
+        Returns:
+            The current square size
+        """
         return self.__size ** 2

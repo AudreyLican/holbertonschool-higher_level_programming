39aaf009eb6b736659130a70513d8836219dafb3
AudreyLican <audrey.lican@gmail.com>
2024-02-23 17:19:42 +0100

    added comments, code style fixed

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
+"""Module for Square class."""
 from models.rectangle import Rectangle
 
 
-"""Define a class Square from Rectangle class"""
-
-
 class Square(Rectangle):
+    """Square class that inherits from Rectangle."""
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a Square instance.
+
+        Args:
+            size (int): Size of the square.
+            x (int): x-coordinate of the square (default is 0).
+            y (int): y-coordinate of the square (default is 0).
+            id (int): ID of the square (default is None).
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Return a string representation of the square."""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                  self.y, self.width)
 
     @property
     def size(self):
+        """Get the size of the square."""
         return self.width
 
     @size.setter
     def size(self, value):
+        """Set the size of the square."""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """Assign arguments to the attributes with args, and kwargs"""
         attributes = ['id', 'size', 'x', 'y']
         if args:
             attrs_plus_value = zip(attributes, args)
@@ -34,9 +45,10 @@ class Square(Rectangle):
                     setattr(self, key, value)
 
     def to_dictionary(self):
+        """Return a dictionary, representation of the rectangle."""
         return {
             'id': self.id,
             'size': self.size,
             'x': self.x,
-            'y' : self.y
+            'y': self.y
         }

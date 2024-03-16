4d655892c58c20278795d790b722f7dc1079d802
AudreyLican <audrey.lican@gmail.com>
2024-03-17 00:01:37 +0100

    Create user idenfied by pwd

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+#!/bin/bash
+-- creates user user_0d_1 with password set to user_0d_1_pwd
+-- if user exist script shouldn't fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';

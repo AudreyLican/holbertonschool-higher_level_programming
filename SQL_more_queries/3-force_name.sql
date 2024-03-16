e3a466992ab39b9814bda5fcc3bb184855337b82
AudreyLican <audrey.lican@gmail.com>
2024-03-17 00:18:35 +0100

    create first table in DB hbtn_0d_2

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+#!/bin/bash
+-- Active: 1710514689193@@127.0.0.1@3306@hbtn_0d_2
+-- create new a table in DB hbtn_0d_2
+CREATE TABLE IF NOT EXISTS force_name(
+    id INT,
+    name VARCHAR(256)
+);

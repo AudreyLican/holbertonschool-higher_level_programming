b4b3966c0cfcaccdff776d7511dc28a91c92a7e5
AudreyLican <audrey.lican@gmail.com>
2024-03-17 20:44:52 +0100

    create table of cities with a foreign key

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+#!/bin/bash
+-- create a new table in MySQL, 2 columns : id attributes: INT, auto indent, PRIMARY KEY, the other name: NOT NULL
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);

90d41eaabb855c57c1a13a06f4cb0ef7e013962b
AudreyLican <audrey.lican@gmail.com>
2024-03-18 10:31:27 +0100

    added privileges request to user_0d_1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,3 +2,5 @@
 -- creates user user_0d_1 with password set to user_0d_1_pwd
 -- if user exist script shouldn't fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

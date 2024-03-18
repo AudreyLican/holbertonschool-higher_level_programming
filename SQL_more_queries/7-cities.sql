faa5626c8843ee61e4ffe153ad3c040adea759d4
AudreyLican <audrey.lican@gmail.com>
2024-03-18 10:37:23 +0100

    added missing UNIQUE to id

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- create a new table in MySQL, 2 columns : id attributes: INT, auto indent, PRIMARY KEY, the other name: NOT NULL
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),

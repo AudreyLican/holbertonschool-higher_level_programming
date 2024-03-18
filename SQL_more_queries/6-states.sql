50c5fed9edd9ba31a541e0a513bd68c7cf17ebd9
AudreyLican <audrey.lican@gmail.com>
2024-03-18 11:00:39 +0100

    delete use

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 #!/bin/bash
 -- create a new table in MySQL, 2 columns : id attributes: INT, auto indent, PRIMARY KEY, the other name: NOT NULL
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE 
 CREATE TABLE IF NOT EXISTS states (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL

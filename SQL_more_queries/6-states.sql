48006b22c35b81aa95a2e6dbed82a101e202a103
AudreyLican <audrey.lican@gmail.com>
2024-03-18 10:33:08 +0100

    added UNIQUE to column id

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,6 +3,6 @@
 -- create a new table in MySQL, 2 columns : id attributes: INT, auto indent, PRIMARY KEY, the other name: NOT NULL
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );

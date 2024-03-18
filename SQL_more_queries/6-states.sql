c038bb6f85e38a00b8f1892221fd5ad9cfcc9b8e
AudreyLican <audrey.lican@gmail.com>
2024-03-18 11:00:08 +0100

    remove unecessary comments

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
--- Active: 1710514689193@@127.0.0.1@3306@hbtn_0d_usa
 #!/bin/bash
 -- create a new table in MySQL, 2 columns : id attributes: INT, auto indent, PRIMARY KEY, the other name: NOT NULL
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+USE 
+CREATE TABLE IF NOT EXISTS states (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );

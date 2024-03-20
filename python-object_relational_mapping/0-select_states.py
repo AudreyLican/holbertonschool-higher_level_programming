74891975e257b0a8f04898127087bc94d4aaed7e
AudreyLican <audrey.lican@gmail.com>
2024-03-20 12:59:14 +0100

    task 0 : script that list all DB usa

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/bin/bash
+import MySQLdb
+from sys import argv
+
+"""
+Listing all states in DB hbtn_0e_0-_usa
+"""
+
+if __name__ == '__main__':
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",   # Host name to DB server
+        port=3306,          # Port number of the DB server
+        user=argv[1],       # Username passed in argument
+        passwd=argv[2],     # Pwd passed in argument
+        db= argv[3])        # DataBase passe in argument
+
+    # create cursor object associated to query,
+    # to acts as a pointer or iterator that allows us to traverse the result set
+    cur = db.cursor()
+
+    # Execute SQL query, to fecth rows from result set in the table "states"
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+
+    # Fetch all rows
+    rows = cur.fetchall()
+
+    # Process data
+    for row in rows:
+        print(row)
+        
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()

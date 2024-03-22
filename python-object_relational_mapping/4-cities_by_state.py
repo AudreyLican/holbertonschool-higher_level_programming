f1d899cf261e4805ffdc7b153637eae8934df521
AudreyLican <audrey.lican@gmail.com>
2024-03-22 21:26:52 +0100

    review query

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     # or iterator that allows us to traverse the result
 
     query = "SELECT cities.id, cities.name, states.name \
-                                FROM cities JOIN states ON cities.state_id \
-                                = states.id ORDER BY cities.id ASC"
+            FROM cities JOIN states ON cities.state_id \
+            = states.id ORDER BY cities.id ASC"
 
     with db.cursor() as cursor:
         cursor.execute(query)

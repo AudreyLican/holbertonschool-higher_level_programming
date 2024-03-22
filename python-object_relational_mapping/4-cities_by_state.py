9874f3148fcfd46f8a5bf08bc212300b2835d192
AudreyLican <audrey.lican@gmail.com>
2024-03-22 21:30:07 +0100

    hot fix

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -24,13 +24,15 @@ if __name__ == "__main__":
             = states.id ORDER BY cities.id ASC"
 
     with db.cursor() as cursor:
-        cursor.execute(query)
+        cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC")
 
-    rows = cursor.fetchall()
+        rows = cursor.fetchall()
 
-    if rows is not None:
-        for row in rows:
-            print(row)
+        if rows is not None:
+            for row in rows:
+                print(row)
 
     # Close all cursors
     cursor.close()

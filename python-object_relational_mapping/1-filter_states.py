cdb533c4762e0e192da847206fdd1fccee91cdf7
AudreyLican <audrey.lican@gmail.com>
2024-03-22 20:30:04 +0100

    create task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Listing all states starting with 'N' in DB hbtn_0e_0-_usa
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",   # Host name to DB server
+        user=argv[1],       # Username passed in argument
+        port=3306,          # Port number of the DB server
+        passwd=argv[2],     # Pwd passed in argument
+        db=argv[3]
+        )         # DataBase passe in argument
+
+    # create cursor object associated to query, to acts as a pointer
+    # or iterator that allows us to traverse the result
+    cur = db.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    # Execute SQL query, to fecth all states starting with 'N'
+    cur.execute(query)
+    
+    rows = cur.fetchall()
+    # Print results
+    for row in rows:
+        print(row)
+
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
